d70ce75f5337ec28a065e99bd7610eddf33ea646
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-07 09:03:48 +0000

    temp code

---
+++ python-hello_world/9-easter_egg.py
@@ -0,0 +1,17 @@
+print("guillaume@ubuntu:~/py/$ ./9-easter_egg.py
+The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.\n
+Namespaces are one honking great idea -- let's do more of those!")
