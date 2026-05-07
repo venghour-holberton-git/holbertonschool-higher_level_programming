fa43f5c8293aa4e762ce89ad818d629c2558fbf2
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-07 05:38:11 +0000

    print float

---
+++ python-hello_world/11-test.py
@@ -0,0 +1,9 @@
+a = 1
+b = 2
+c = 10
+def add(a, b, c):
+    c = a + b
+    print(f"function c res {c}")
+    return c
+
+print(f"{c} local python")
---
+++ python-hello_world/4-print_float.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+c = 5.2;
+print(c);
