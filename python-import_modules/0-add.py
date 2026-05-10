bb038b3b17d14289d843ee6cf80f8b3ba69aaebf
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-10 07:00:36 +0000

    add import guard

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,8 @@
 
 add = __import__('add_0').add
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
-print("{} + {} = {}".format(a, b, add(a, b)))
