0b9587681882d0b67617ef20a6c89df34e225fa1
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-10 05:09:46 +0000

    fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,4 +2,10 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        print(f"{'FizzBuzz' if i % 3 == 0 and i % 5 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 else i} ", end="")
+        if (i % 3 == 0 and i % 5 == 0):
+            print("FizzBuzz ")
+        elif (i % 3 == 0):
+            print("Fizz ")
+        elif (i % 5 == 0):
+            print("Buzz ")
+        else print(f"{i} ")
