92e92e7c505773cda0005eabdc351dfee12d6b37
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-10 09:52:10 +0000

    minor fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,10 @@
 
 if __name__ == "__main__":
     import sys
-    print(f"{len(sys.argv) - 1} arguments{':' if len(sys.argv) - 1 != 0 else '.'}")
+    arg_length = len(sys.argv) - 1
+    s_text = '' if arg_length == 1 else 's'
+    extra_text = ':' if len(sys.argv) - 1 != 0 else '.'
+    print(f"{arg_length} argument{s_text}{extra_text}")
     if len(sys.argv) - 1 != 0:
         index = 1
         for s in sys.argv[1:]:
