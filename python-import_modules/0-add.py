61b8ca6e166b630c46b70c1feac267ad058c087b
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-10 07:07:12 +0000

    fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-add = __import__('add_0').add
+import add_0
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
 
