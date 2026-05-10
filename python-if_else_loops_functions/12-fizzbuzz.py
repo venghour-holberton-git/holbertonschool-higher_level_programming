41331be2565c4a1221cf1428ff2e33d36e6de868
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-10 05:05:22 +0000

    FizzBuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(1, 101):
+        print(f"{'FizzBuzz' if i % 3 == 0 and i % 5 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 else i} ", end="")
+        if i == 100:
+            print();
+fizzbuzz();
