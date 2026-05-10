e8060734d9764c3ef285ef0518ae1d4db50c459e
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-10 03:57:34 +0000

    fix

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    last_digit = number % 10 if number >= 0 else -(-number % 10)
+    print(f"{last_digit}", end="")
+    return last_digit
+
+print_last_digit(-105)
+i = print_last_digit(1987)
+print(f"{i}")
