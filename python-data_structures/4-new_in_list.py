d5add203d4840ec0b8a048104ed63298d6b3ed9f
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-11 13:46:52 +0000

    replace element in a new list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return new_list
+    new_list[idx] = element
