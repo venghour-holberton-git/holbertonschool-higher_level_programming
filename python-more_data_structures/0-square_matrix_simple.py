c6c327e5ab522954d7d193ff2d045c52aa6e2d36
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-14 02:26:14 +0000

    square matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+        for number in row:
+            new_row.append(number * number)
+        new_matrix.append(new_row)
+    return new_matrix
