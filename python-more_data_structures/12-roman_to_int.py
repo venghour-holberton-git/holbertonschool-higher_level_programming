cebae2a45e0d4060005a70909ccf75ed745060eb
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-14 14:04:59 +0000

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,12 +4,16 @@ def roman_to_int(roman_string):
     i = 0
     total = 0
     temp_value = 0
+    last_char = ''
 
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     for c in roman_string:
         if c == 'I':
             temp_value += 1
+            if temp_value == 3:
+                total += temp_value
+            last_char = 'I'
             continue
         if c == 'V' and temp_value == 0:
             total += 5
@@ -28,19 +32,29 @@ def roman_to_int(roman_string):
             temp_value = 0
             continue
         if c == 'L':
-            total += 50
+            add_value = 50
+            if last_char == 'X':
+                add_value -= 10
+                last_char = 'L'
+            total += add_value
             temp_value = 0
             continue
         if c == 'C':
-            total += 100
+            add_value = 100
+            if last_char == 'X':
+                add_value -= 10
+                last_char = 'C'
+            total += add_value
             temp_value = 0
             continue
         if c == 'D':
-            total += 500
+            add_value = 500
+            if last_char == 'C':
+                add_value -= 100
+                last_char = 'D'
+            total += add_value
             temp_value = 0
             continue
-        if temp_value == 3:
-            total += temp_value
     if temp_value != 0:
         total += temp_value
     return total
