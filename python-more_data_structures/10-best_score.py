d09cfa7cf7c6f5f5dec1c161243abf0e2d50682b
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-14 11:27:41 +0000

    fix

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None or a_dictionary == {}:
+    if a_dictionary is None or a_dictionary == {}:
         return None
-    sorted_score = [k for k, v in sorted(a_dictionary.items(), key = lambda item: item[1])]
-    return sorted_score[-1]
+    get_value = lambda item: item[1]
+    sorted_score = sorted(a_dictionary.items(), key = get_value)
+    new_array = [k for k, v in sorted_score]
+    return new_array[-1]
