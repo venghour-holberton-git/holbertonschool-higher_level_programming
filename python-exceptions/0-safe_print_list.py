2a00173ffdaae92a97d0c6509564bb1b168b2208
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-15 02:03:08 +0000

    fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range (x):
+    count = 0
+    for i in range (x):
+        try:
             print(my_list[i], end="")
-        print()
-    except:
-        print("something is wrong, maybe x is out of range")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
