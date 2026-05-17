181bce33d8e6841f09f30731dd4ccd4c26dc0914
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-17 13:25:11 +0000

    fix

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""say name module"""
+
 
 def say_my_name(first_name, last_name=""):
+    """say name"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
