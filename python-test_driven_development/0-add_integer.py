0b1280da93888b2edb5283226d6bd9e434d1d362
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-17 08:18:57 +0000

    fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,10 +3,17 @@
 
 def add_integer(a, b=98):
     """Add two integers and return the result."""
-    if not isinstance(a, (int, float)):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, (int, float)):
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    # detect infinity without importing modules
+    if a == float('inf') or a == float('-inf'):
+        raise TypeError("a must be an integer")
+
+    if b == float('inf') or b == float('-inf'):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
