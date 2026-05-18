6c5c05efc2f89aa10a181336e4f3de5d74985d98
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-18 11:28:08 +0000

    fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-"""square module"""
 
 class Square:
-    """square class"""
+    """Defines a square."""
+
     def __init__(self, size=0):
-        self.__size = size
+        """Initialize square with size."""
+        self.size = size
 
     @property
     def size(self):
-        """get size"""
+        """Retrieve the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """set size"""
-        if not isinstance(value, int):
+        """Set the size with validation."""
+
+        if type(value) is not int:
             raise TypeError("size must be an integer")
+
         if value < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = value
 
     def area(self):
-        """return square area"""
-        return size ** 2
+        """Return the current square area."""
+        return self.__size ** 2
