015cce86387ddfb08a96b8d5cfe60509c11e6881
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-18 11:20:05 +0000

    fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,7 +5,11 @@ class Square:
     """square class"""
     def __init__(self, size=0):
         self.__size = size
+
+    @property
     def size(self):
         return self.__size
+
+    @size.setter
     def size(self, value):
         self.__size = value
