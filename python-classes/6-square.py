2a06db985ea780941137f6a3f5d05b16736fa6b6
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-18 12:06:35 +0000

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""square class module"""
+"""square module"""
 
 
 class Square:
     """square class"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
         return self.__size
+    @property
     def position(self):
         return self.__position
 
