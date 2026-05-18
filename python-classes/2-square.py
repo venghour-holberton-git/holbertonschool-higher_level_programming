369e4ea613817dc66adf8dd265e00fd692afef14
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-18 11:00:09 +0000

    square 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize square with validated size."""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
