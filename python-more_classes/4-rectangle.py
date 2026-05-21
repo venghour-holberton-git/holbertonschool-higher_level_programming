f926c285e09bd94969aa6dfe0e78454c85b098a7
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-21 15:07:18 +0000

    fix

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -57,9 +57,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
-if __name__ == "__main__":
-    rec1 = Rectangle(4, 5)
-    print(repr(rec1))
-    rec3 = eval(repr(rec1))
-    print(str(rec3))
