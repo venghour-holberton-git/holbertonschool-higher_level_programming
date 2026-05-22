04c75655d16c8a742b704e91d456d3d0927b1abc
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-22 14:53:10 +0000

    more test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,5 +7,14 @@ class TestMaxInteger(unittest.TestCase):
     def test_case_1(self):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
+    def test_case_2(self):
+        self.assertEqual(max_integer([1, 3, 2, 4]),4)
+    
+    def test_case_3(self):
+        self.assertEqual(max_integer([8, 3, 2, 5]),8)
+
+    def test_case_4(self):
+        self.assertEqual(max_integer([3]),3)
+
 if __name__ == "__main__":
     unittest.main()
