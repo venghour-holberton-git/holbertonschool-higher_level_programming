4ecb58a74c5e54d023ed58e2bc56746c79dd7ebc
venghour-holberton-git <13067@holbertonstudents.com>
2026-05-25 12:01:08 +0000

    f

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""function module"""
 
 MyList = __import__("1-my_list").MyList
 
 def is_same_class(obj, a_class):
+    """return if same as class"""
     return type(obj) is a_class
