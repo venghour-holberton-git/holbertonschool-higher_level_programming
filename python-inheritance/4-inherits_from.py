85533dfda8b0228dd1f4ef2c9e2654559404a41d
Venghour-holberton-git <13067@holbertonstudents.com>
2026-05-26 12:38:46 +0000

    only inherit from

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Checks if an object is an instance of a subclass."""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a class that inherited
+    from a_class, directly or indirectly.
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
