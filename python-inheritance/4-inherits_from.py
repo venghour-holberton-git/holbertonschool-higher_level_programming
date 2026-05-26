84b6728d6a2f98ed2a70e943a373b24645779fa8
Venghour-holberton-git <13067@holbertonstudents.com>
2026-05-26 12:39:36 +0000

    f

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """Return True if obj is an instance of a class that inherited
     from a_class, directly or indirectly.
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
