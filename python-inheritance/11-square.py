8937f0b7c47a0e005f89ad1603e057428aec80d1
Venghour-holberton-git <13067@holbertonstudents.com>
2026-05-27 13:14:38 +0000

    square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class."""
+
+    def __init__(self, size):
+        """Initialize a square."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return the string representation of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
