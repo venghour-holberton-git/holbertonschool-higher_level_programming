16a2dfc559aa01a890c06382638bde3d0e52e8d7
Venghour-holberton-git <13067@holbertonstudents.com>
2026-05-29 02:59:07 +0000

    duck typing

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+import math
+"""Shape module"""
+
+
+class Shape(ABC):
+    """shape class"""
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    """Circle class"""
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @radius.setter
+    def radius(self, value):
+        self.__radius =  value
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (self.width + self.height) * 2
+
+def shape_info(shape):
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
+
+if __name__ == "__main__":
+    circle = Circle(radius=5)
+    rectangle = Rectangle(width=4, height=7)
+
+    shape_info(circle)
+    shape_info(rectangle)
