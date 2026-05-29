cbe9b5ef0a0741341977e2ff970af937fe033678
Venghour-holberton-git <13067@holbertonstudents.com>
2026-05-29 09:48:16 +0000

    verbose

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""List module"""
+
+
+class VerboseList(list):
+    """VerboseList class"""
+
+    def __init__(self):
+        super().__init__()
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, item):
+        super().extend(item)
+        print(f"Extended the list with [{len(item)}] items.") 
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index = None):
+        popIndex = index if index != None else len(self) - 1
+        print(f"Popped [item] from the list.")
+        super().pop(popIndex)
+
+if __name__ == "__main__":
+    list_2 = VerboseList()
+    list_2.append("bnn")
+    list_2.extend(["vv", "dffdd"])
+    print(list_2)
+    list_2.remove('vv')
+    print(list_2)
