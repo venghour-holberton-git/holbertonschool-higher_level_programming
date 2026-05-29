5fdbc558a47020c7b3dde05ed5b328cdfc543980
Venghour-holberton-git <13067@holbertonstudents.com>
2026-05-29 10:47:20 +0000

    fix

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+class CountedIterator():
+    def __init__(self, items):
+        self.__iterator = iter(items)
+        self.__count = 0
+
+    def get_count(self):
+        return self.__count
+    def __next__(self):
+        next(self.__iterator)
+        self.__count += 1
+
+if __name__ == "__main__":
+    l1 = CountedIterator([1, 2, 3, 4])
+    print(l1.get_count())
+    next(l1)
+    print(l1.get_count())
+    try:
+        while True:
+            item = next(l1)
+            print(f"Got {item}, total {l1.get_count()} items iterated.")
+    except StopIteration:
+        print("No more items.")
