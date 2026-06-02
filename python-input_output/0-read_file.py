d238cc3a04110a2bee53e366b509acc377f814ad
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-02 06:28:01 +0000

    f

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""read file module"""
 
 def read_file(filename=""):
+    """read file function"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print (read_data)
