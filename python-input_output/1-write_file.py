7848e67eac2fe8cd31bf4c77e1f66dcc91013196
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-02 06:59:26 +0000

    f

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """wrtie function"""
-    with open(file, encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         f.write(text)
