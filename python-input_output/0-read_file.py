684570cb98cf2e21e0b47de9e80e9c2fc61ea4ca
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-02 06:29:33 +0000

    f

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@ def read_file(filename=""):
     """read file function"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-        print (read_data)
+        print (read_data, end="")
