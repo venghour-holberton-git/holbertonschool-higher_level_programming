de90e8e1a49f51b9166c115e60cb1a5c7d5528a6
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-02 07:07:09 +0000

    f

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""wite module"""
+"""Module that writes a string to a UTF-8 text file."""
 
 
-def write_file(filename="", "w", text=""):
-    """wrtie function"""
-    with open(filename, encoding="utf-8") as f:
-        f.write(text)
+def write_file(filename="", text=""):
+    """Write text to a file and return the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
