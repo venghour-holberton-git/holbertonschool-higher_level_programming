c4aa425cfe23e67ca59ccdeba4c089fc9348fcda
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-02 07:21:42 +0000

    more student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the instance."""
+        if type(attrs) is list:
+            result = {}
+            for attr in attrs:
+                if type(attr) is str and hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+
+        return self.__dict__
