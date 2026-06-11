655552fde8bf177365eb3465cba66e4f42a5cf62
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-11 20:22:29 +1000

    http request

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import http.server
+import json
+
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            j = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(j).encode())
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Not Found")
+
+def run(server_class=http.server.HTTPServer, handler_class=RequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+run()
