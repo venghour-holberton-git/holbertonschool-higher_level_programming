6ee5b5bd3ed346dc65fa559b8c18f95159b0965b
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-11 19:51:37 +1000

    first api request

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,30 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(r.status_code)
+    data = None
+    if r.status_code == 200:
+        data = r.json()
+        for d in data:
+            print(d["title"])
+            
+def fetch_and_save_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if r.status_code == 200:
+        j = r.json()
+        data = [{"id": item["id"],
+                 "title": item["title"],
+                 "body": item["body"]
+                 } 
+                 for item in j
+                ]
+        with open("posts.csv", "w", newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data)
+
+if __name__ == "__main__":
+    fetch_and_save_posts()
