ef755e8333755897f6da26f87562e64e0c546141
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-11 20:31:44 +1000

    f

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -23,11 +23,9 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b"OK")
         else:
             self.send_response(404)
-            self.send_header('Content-Type', 'application/json')
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
-
-            error = {"error": "Not Found"}
-            self.wfile.write(json.dumps(error).encode())
+            self.wfile.write(b"Endpoint not found")
 
 def run(server_class=http.server.HTTPServer, handler_class=RequestHandler):
     server_address = ('', 8000)
