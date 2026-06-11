6163d325ef1ca7a710457d0e85ac2765613c5ae9
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-11 22:33:26 +1000

    f

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -37,7 +37,7 @@ def add_user():
         if data["username"] in all_users:
             return jsonify({"error":"Username already exists"}), 409
         all_users[data["username"]] = data
-        return jsonify(all_users)
+        return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
