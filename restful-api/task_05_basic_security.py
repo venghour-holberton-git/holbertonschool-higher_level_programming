c8eafe3937fe9ac5c81fdaf7d5d9a77db4fd9444
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-12 19:45:57 +1000

    auth

---
+++ restful-api/task_05_basic_security.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, {}!".format(auth.current_user())
+
+if __name__ == '__main__':
+    app.run()
+
+# from flask import Flask
+# from flask import jsonify
+# from flask import request
+
+# from flask_jwt_extended import create_access_token
+# from flask_jwt_extended import get_jwt_identity
+# from flask_jwt_extended import jwt_required
+# from flask_jwt_extended import JWTManager
+
+# app = Flask(__name__)
+
+# # Setup the Flask-JWT-Extended extension
+# app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+# jwt = JWTManager(app)
+
+
+# # Create a route to authenticate your users and return JWTs. The
+# # create_access_token() function is used to actually generate the JWT.
+# @app.route("/login", methods=["POST"])
+# def login():
+#     username = request.json.get("username", None)
+#     password = request.json.get("password", None)
+#     if username != "test" or password != "test":
+#         return jsonify({"msg": "Bad username or password"}), 401
+
+#     access_token = create_access_token(identity="apple")
+#     return jsonify(access_token=access_token)
+
+
+# # Protect a route with jwt_required, which will kick out requests
+# # without a valid JWT present.
+# @app.route("/protected", methods=["GET"])
+# @jwt_required()
+# def protected():
+#     # Access the identity of the current user with get_jwt_identity
+#     current_user = get_jwt_identity()
+#     return jsonify(logged_in_as=current_user), 200
+
+# if __name__ == "__main__":
+#     app.run()
