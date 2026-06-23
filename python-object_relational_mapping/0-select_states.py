5a9359b55c527cff7d0ad3d5131162ee86fef904
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-23 22:11:48 +1000

    f

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
+"""query data"""
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
+    argv_params = sys.argv
+    if len(argv_params) < 4:
+        raise ValueError("Not enough system parameter provided")
+    db = MySQLdb.connect(host='localhost', user=argv_params[1], passwd=argv_params[2], db=argv_params[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    for row in cur.fetchall():
-        print(row)
-
-    cur.close()
-    db.close()
+    rows = cur.fetchall()
+    for row in rows:
+        print (row)
