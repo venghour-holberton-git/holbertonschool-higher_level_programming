817cab636f7ea97dd0bbc1a33369ea79b49a622d
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-23 22:16:17 +1000

    f

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,18 @@ if __name__ == "__main__":
     argv_params = sys.argv
     if len(argv_params) < 4:
         raise ValueError("Not enough system parameter provided")
-    db = MySQLdb.connect(host='localhost', user=argv_params[1], passwd=argv_params[2], db=argv_params[3])
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print (row)
+
+    cur.close()
+    db.close()
