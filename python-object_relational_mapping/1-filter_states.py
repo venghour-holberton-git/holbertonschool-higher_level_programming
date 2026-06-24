6d43a8a5bcee3a8d68eb2e399c1de87ed89e9791
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-24 11:56:12 +1000

    f

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
 
     rows = cur.fetchall()
 
