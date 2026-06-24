c87e32b50d4c086a315616029c1729fb82dafbaa
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-24 11:58:29 +1000

    f

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf-8"
     )
 
     cur = db.cursor()
