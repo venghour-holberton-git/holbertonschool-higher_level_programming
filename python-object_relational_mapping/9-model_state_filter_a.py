82c4a986dd0bc8ee8f2dec06631712e5a955e8a1
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-25 14:41:51 +1000

    f

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A script that lists all State objects from hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).where(State.name.contains('a')).order_by(State.id).all()
+    if states is None:
+        print("Nothing")
+    else:
+        # print("{}: {}".format(states.id, states.name))
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+
+    session.close()
