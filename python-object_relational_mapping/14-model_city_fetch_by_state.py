66c3205769605d7d76b4de69ebcf76a7f01097c9
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-25 15:16:14 +1000

    f

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Lists all City objects with their State names"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}"
+        f"@localhost:3306/{sys.argv[3]}",
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = session.query(State, City)\
+                     .join(City, State.id == City.state_id)\
+                     .order_by(City.id)\
+                     .all()
+
+    for state, city in results:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    session.close()
