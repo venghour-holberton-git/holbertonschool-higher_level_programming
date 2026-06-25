d1e4f7112ff021b7e1b07edde692e57a6b4e58c5
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-25 13:12:02 +1000

    f

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A script that lists all cities of a state from hbtn_0e_4_usa"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT cities.id, cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC",
+        (sys.argv[4],)
+    )
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
