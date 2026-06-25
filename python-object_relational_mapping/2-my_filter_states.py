bb817f018e47aa707a9e40a8a21a8a9ae2c10a72
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-25 12:37:46 +1000

    f

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
 
     search_name = sys.argv[4]
     cur.execute("SELECT * FROM states "
-                "WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(search_name))
+                "WHERE name LIKE BINARY '{}' "
+                "ORDER BY id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
     # print(query_rows)
     for row in query_rows:
