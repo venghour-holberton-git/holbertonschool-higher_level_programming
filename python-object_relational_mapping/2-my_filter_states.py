660175964ebb7317863173030423caf9ea758336
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-25 12:29:37 +1000

    f

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for ORM to list all states from hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+
+    search_name = sys.argv[4]
+    cur.execute("SELECT * FROM states "
+                "WHERE name LIKE BINARY '{}'".format(search_name))
+    query_rows = cur.fetchall()
+    # print(query_rows)
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
