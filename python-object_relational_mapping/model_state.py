8126ad4a963af65b6309ec73accda0ef1cf1cdb9
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-25 13:18:13 +1000

    f

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,46 +1,16 @@
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, DECIMAL, select, Boolean, ForeignKey
-from sqlalchemy.orm import declarative_base, Session, relationship
-import copy
+#!/usr/bin/python3
+"""Module containing State class definition and Base instance"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-class User(Base):
-    __tablename__ = "employees"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    rating = Column(DECIMAL(10, 20))
-    quantity = Column(Integer)
-    # addresses = relationship(
-    #      "User2", back_populates="user", cascade="all, delete-orphan"
-    #     )
-    
-class User2(Base):
-    __tablename__ = "ep"
 
 
-    id = Column(Integer, ForeignKey("employees.id"), primary_key=True, nullable=False)
-    is_man = Column(Boolean)
-    # user = relationship("User", back_populates="addresses")
+class State(Base):
+    """State class linked to states table"""
 
-engine = sqlalchemy.create_engine("mysql+mysqldb://root:@localhost/m_database")
+    __tablename__ = "states"
 
-Base.metadata.create_all(engine)
-session = Session(engine)
-# mango_boy = User(id=19, name= "g23g_po22p", rating= 10.2, quantity=23)
-# session.add(mango_boy)
-# session.commit()
-users = session.execute(select(User, User2).join(User2)).all()
-# users1 = [copy.copy(u[0]) for u in users]
-u_1 = session.query(User).filter_by(id = 15).first()
-# u_1.name = "coconutZen"
-# session.commit()
-# print(users[0][0].__dict__)
-for u, p in users:
-    print(u.__dict__, p.__dict__)
-# for u in users:
-#     print(u.__dict__)
-#     session.delete(u)
-# session.commit()
-# session.delete(u_1)
-# session.commit()
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
